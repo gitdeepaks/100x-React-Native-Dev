a3c573794f15786982e1d701c0642927a460e194
gitdeepaks <deepaksankhyan92@outlook.com>
2025-03-14 22:31:12 +0530

    added some typeInputs in workout screen

--- my-fitness/src/components/logger/SetItems.tsx
+++ my-fitness/src/components/logger/SetItems.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, TextInput } from "@/components/general/Themed";
 import { ExerciseSet } from "@/types/models";
+import { StyleSheet } from "react-native";
 
 type SetItems = {
   index: number;
@@ -8,10 +9,57 @@ type SetItems = {
 };
 
 export default function SetItems({ index, set }: SetItems) {
+  const [weight, setWeight] = React.useState(set.weight?.toString() || "");
+  const [reps, setReps] = React.useState(set.reps?.toString() || "");
+
+  const hanldeWeightChange = () => {
+    console.warn("handle weight change", weight);
+  };
+
+  const handleRepsChange = () => {
+    console.warn("handle reps change", reps);
+  };
+
   return (
-    <View>
-      <Text>{index + 1}</Text>
-      <TextInput placeholder="50" />
+    <View style={styles.container}>
+      <Text style={styles.setNumber}>{index + 1}</Text>
+      <TextInput
+        placeholder="50"
+        value={weight}
+        onChangeText={setWeight}
+        style={styles.input}
+        keyboardType="numeric"
+        onBlur={hanldeWeightChange}
+      />
+      <TextInput
+        placeholder="8"
+        value={reps}
+        onChangeText={setReps}
+        style={styles.input}
+        keyboardType="numeric"
+        onBlur={handleRepsChange}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    marginVertical: 10,
+    gap: 5,
+    alignItems: "center",
+  },
+  setNumber: {
+    marginRight: "auto",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  input: {
+    width: 60,
+    padding: 5,
+    paddingVertical: 7,
+    fontSize: 16,
+    textAlign: "center",
+  },
+});
